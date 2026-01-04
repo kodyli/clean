2070c649eb3c18e15b755c3044fe3973fc3ae3ba
= <=>
2026-01-04 08:46:02 -0500

    format code

--- clean-application/src/main/java/li/yansan/clean/application/client/ClientResponse.java
+++ clean-application/src/main/java/li/yansan/clean/application/client/ClientResponse.java
@@ -9,23 +9,28 @@ import li.yansan.clean.commons.validation.Validator;
  * Represents a standardized response from a third-party Client.
  *
  * <p>
- * This record encapsulates the result of an external API call, ensuring that the returned
+ * This record encapsulates the result of an external API call, ensuring that
+ * the returned
  * data is valid and not null.
  *
  * <p>
  * <b>Responsibilities:</b>
  *
  * <ul>
- * <li><b>Encapsulation:</b> Holds the {@code body} of the response returned by the
+ * <li><b>Encapsulation:</b> Holds the {@code body} of the response returned by
+ * the
  * client.
- * <li><b>Validation:</b> Enforces that the {@code body} is not null and satisfies any
- * constraints defined on it (e.g., via Bean Validation annotations) upon instantiation.
- * <li><b>Transformation:</b> Provides constructors to facilitate the conversion of
+ * <li><b>Validation:</b> Enforces that the {@code body} is not null and
+ * satisfies any
+ * constraints defined on it (e.g., via Bean Validation annotations) upon
+ * instantiation.
+ * <li><b>Transformation:</b> Provides constructors to facilitate the conversion
+ * of
  * different data types into the required domain object ({@code UBody}).
  * </ul>
  *
  * @param <UBody> the type of the response body
- * @param body the body of the response; must not be null
+ * @param body    the body of the response; must not be null
  */
 public record ClientResponse<UBody>(@Valid @NotNull UBody body) {
 	public ClientResponse(UBody body) {
@@ -33,7 +38,7 @@ public record ClientResponse<UBody>(@Valid @NotNull UBody body) {
 		validate();
 	}
 
-	public ClientResponse(Convertible<UBody> body) {
+	public ClientResponse(@Valid @NotNull Convertible<UBody> body) {
 		this(body.convert());
 	}
 

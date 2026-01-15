f0a051438118acde1a6105b0950df5c966f71062
= <=>
2026-01-14 22:11:19 -0500

    format code

--- clean-platform/src/main/java/li/yansan/clean/platform/MessengerBase.java
+++ clean-platform/src/main/java/li/yansan/clean/platform/MessengerBase.java
@@ -1,5 +1,7 @@
 package li.yansan.clean.platform;
 
+import java.util.Objects;
+
 import li.yansan.clean.application.Actor;
 import li.yansan.clean.application.messaging.Messenger;
 import li.yansan.clean.application.messaging.MessengerRequest;
@@ -9,9 +11,7 @@ import li.yansan.clean.commons.validation.Validator;
 public abstract class MessengerBase<TI, TO, UPayload, UBody> implements Messenger<UPayload, UBody> {
 
 	public MessengerResponse<UBody> send(MessengerRequest<UPayload> request) {
-		if (request == null) {
-			throw new IllegalArgumentException("MessengerRequest can not be null.");
-		}
+		Objects.requireNonNull(request, "MessengerRequest can not be null.");
 		TI input = convertPayload(request.sender(), request.payload());
 		validate(input);
 		TO output = process(input);
